870e7137182357c3ae5f2d8f34b1b633fcd3f777
Denis Perevalov <denis.perevalov@gmail.com>
2018-02-06 23:34:50 -0600

    replace behavior id

--- coreml_to_json.py
+++ coreml_to_json.py
@@ -1,5 +1,26 @@
 from protobuf_to_dict import protobuf_to_dict
 
+# From https://apple.github.io/coremltools/coremlspecification/sections/TreeEnsembles.html
+TreeNodeBehavior = [
+            'BranchOnValueLessThanEqual',
+            'BranchOnValueLessThan',
+            'BranchOnValueGreaterThanEqual',
+            'BranchOnValueGreaterThan',
+            'BranchOnValueEqual',
+            'BranchOnValueNotEqual',
+            'LeafNode'
+]
+
+def _replace_behavior(json):
+    if isinstance(json, dict):
+        for k,v in json.items():
+            if k == "nodeBehavior":
+                json[k] = TreeNodeBehavior[v]
+            _replace_behavior(v)
+    if isinstance(json, list):
+        for ijson in json:
+            _replace_behavior(ijson)
+
 def coreml_to_json(file_or_model):
     """Converts CoreML to Json"""
     
@@ -14,6 +35,8 @@ def coreml_to_json(file_or_model):
         model = file_or_model
     
     model_json = protobuf_to_dict(model)
+    _replace_behavior(model_json)
+    
     return model_json
 
 
